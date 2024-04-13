84b255dc56da1b58b29d284dee51fe522a06b2cc
Chinmay Gumgaonkar <chinmay.gumgaonkar@innovapptive.com>
2024-04-14 02:01:48 +0530

    Updates

---
+++ common/src/events/ticket-updated-event.ts
@@ -0,0 +1,10 @@
+import { Subject } from "./subjects";
+export interface TicketUpdatedEvent {
+  subject: Subject.TicketUpdate;
+  data: {
+    id: string;
+    title: string;
+    price: number;
+    userId: string;
+  };
+}
