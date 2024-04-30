8753c39ca9b597c3a30f7aa103ad1ef0f009d3c5
Chinmay Gumgaonkar <chinmay.gumgaonkar@innovapptive.com>
2024-05-01 01:31:56 +0530

    Added listner in ticket after order creation and order updation

---
+++ tickets/src/events/listners/order-cancelled-listner.ts
@@ -0,0 +1,32 @@
+import {
+	Subject,
+	listener,
+	orderCancelledEvent,
+} from "@chinmayticketsinno/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/tickets";
+import { TicketUpdatedPublisher } from "../publisher/ticket-updated-publisher";
+export class orderCancelledListner extends listener<orderCancelledEvent> {
+	queueGroupName: string = queueGroupName;
+	subject: Subject.orderCancelled = Subject.orderCancelled;
+
+	async onMessage(data: orderCancelledEvent["data"], msg: Message) {
+		const ticket = await Ticket.findById(data.ticket.id);
+		if (!ticket) {
+			throw new Error("Ticket not found");
+		}
+		ticket.set({ orderId: undefined });
+		await ticket.save();
+
+		await new TicketUpdatedPublisher(this.client).publish({
+			id: ticket.id,
+			orderId: ticket.orderId,
+			userId: ticket.userId,
+			price: ticket.price,
+			title: ticket.title,
+			version: ticket.version,
+		});
+		msg.ack();
+	}
+}
