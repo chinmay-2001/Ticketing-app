4862a85462200f5ca5461d7f6b62e20f4bd39f2a
Chinmay Gumgaonkar <chinmay.gumgaonkar@innovapptive.com>
2024-05-01 00:51:58 +0530

    Updates

--- common/src/events/base-listener.ts
+++ common/src/events/base-listener.ts
@@ -1,48 +1,48 @@
 import nats, { Message, Stan } from "node-nats-streaming";
 import { Subject } from "./subjects";
 interface Event {
-  subject: Subject;
-  data: any;
+	subject: Subject;
+	data: any;
 }
 
 export abstract class listener<T extends Event> {
-  abstract queueGroupName: string;
-  abstract subject: T["subject"];
-  abstract onMessage(data: T["data"], msg: Message): void;
-  private client: Stan;
-  protected ackWait = 5 * 1000;
+	abstract queueGroupName: string;
+	abstract subject: T["subject"];
+	abstract onMessage(data: T["data"], msg: Message): void;
+	protected client: Stan;
+	protected ackWait = 5 * 1000;
 
-  constructor(client: Stan) {
-    this.client = client;
-  }
+	constructor(client: Stan) {
+		this.client = client;
+	}
 
-  subscriptionOptions() {
-    return this.client
-      .subscriptionOptions()
-      .setDeliverAllAvailable()
-      .setManualAckMode(true)
-      .setAckWait(this.ackWait)
-      .setDurableName(this.queueGroupName);
-  }
+	subscriptionOptions() {
+		return this.client
+			.subscriptionOptions()
+			.setDeliverAllAvailable()
+			.setManualAckMode(true)
+			.setAckWait(this.ackWait)
+			.setDurableName(this.queueGroupName);
+	}
 
-  listen() {
-    const subscription = this.client.subscribe(
-      this.subject,
-      this.queueGroupName,
-      this.subscriptionOptions()
-    );
+	listen() {
+		const subscription = this.client.subscribe(
+			this.subject,
+			this.queueGroupName,
+			this.subscriptionOptions()
+		);
 
-    subscription.on("message", (msg: Message) => {
-      console.log(`Message Received: ${this.subject} / ${this.queueGroupName}`);
-      const parsedData = this.parseMessage(msg);
-      this.onMessage(parsedData, msg);
-    });
-  }
+		subscription.on("message", (msg: Message) => {
+			console.log(`Message Received: ${this.subject} / ${this.queueGroupName}`);
+			const parsedData = this.parseMessage(msg);
+			this.onMessage(parsedData, msg);
+		});
+	}
 
-  parseMessage(msg: Message) {
-    const data = msg.getData();
-    return typeof data === "string"
-      ? JSON.parse(data)
-      : JSON.parse(data.toString("utf-8"));
-  }
+	parseMessage(msg: Message) {
+		const data = msg.getData();
+		return typeof data === "string"
+			? JSON.parse(data)
+			: JSON.parse(data.toString("utf-8"));
+	}
 }
--- common/src/events/base-publisher.ts
+++ common/src/events/base-publisher.ts
@@ -2,28 +2,28 @@ import { Stan } from "node-nats-streaming";
 import { Subject } from "./subjects";
 import { convertToObject } from "typescript";
 interface Event {
-  subject:
-    | Subject.TicketCreated
-    | Subject.TicketUpdate
-    | Subject.OrderCreated
-    | Subject.orderCancelled;
-  data: any;
+	subject:
+		| Subject.TicketCreated
+		| Subject.TicketUpdate
+		| Subject.OrderCreated
+		| Subject.orderCancelled;
+	data: any;
 }
 export abstract class Publisher<T extends Event> {
-  abstract subject: T["subject"];
-  private client: Stan;
-  constructor(client: Stan) {
-    this.client = client;
-  }
+	abstract subject: T["subject"];
+	protected client: Stan;
+	constructor(client: Stan) {
+		this.client = client;
+	}
 
-  publish(data: T["data"]): Promise<void> {
-    return new Promise((resolve, reject) => {
-      this.client.publish(this.subject, JSON.stringify(data), (err) => {
-        if (err) return reject(err);
+	publish(data: T["data"]): Promise<void> {
+		return new Promise((resolve, reject) => {
+			this.client.publish(this.subject, JSON.stringify(data), (err) => {
+				if (err) return reject(err);
 
-        console.log("event subject to subject", this.subject);
-        resolve();
-      });
-    });
-  }
+				console.log("event subject to subject", this.subject);
+				resolve();
+			});
+		});
+	}
 }
