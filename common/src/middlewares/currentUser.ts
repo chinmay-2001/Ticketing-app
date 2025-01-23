a1bd4322a5a7c6d63c48aff8bda9e3cdeafee632
Chinmay <chinmaygumngaonkar2@gmail.com>
2025-01-23 15:01:16 +0530

    Updates

--- common/src/middlewares/currentUser.ts
+++ common/src/middlewares/currentUser.ts
@@ -18,16 +18,33 @@ export const currentUser = (
   res: Response,
   next: NextFunction
 ) => {
-  if (!req.session?.jwt) {
-    return next();
+  const authHeader = req.headers.authorization;
+  console.log(req.headers);
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return next(); // No token provided; proceed without attaching `currentUser`
   }
-
+  const token = authHeader.split(" ")[1];
   try {
     const payload = jwt.verify(
-      req.session.jwt,
-      process.env.JWT_KEY!
+      token,
+      process.env.ACCESS_TOKEN_SECRET!
     ) as userPayload;
-    req.currentUser = payload;
-  } catch (err) {}
+    req.currentUser = payload; // Attach the decoded payload to the `req` object
+  } catch (err) {
+    // Token verification failed; proceed without attaching `currentUser`
+  }
+
+  // if (!req.session?.jwt) {
+  //   return next();
+  // }
+
+  // try {
+  //   const payload = jwt.verify(
+  //     req.session.jwt,
+  //     process.env.JWT_KEY!
+  //   ) as userPayload;
+  //   req.currentUser = payload;
+  // } catch (err) {}
+
   next();
 };
